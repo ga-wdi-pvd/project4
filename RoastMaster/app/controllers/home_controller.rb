5fda2235208034260d8d89a955bbec5a5197813d
ahnuce <ahnuce@gmail.com>
2017-02-08 17:19:04 -0500

    disqus comment system added to roasts

--- RoastMaster/app/controllers/home_controller.rb
+++ RoastMaster/app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   # load_and_authorize_resource :class => RoastsController
+before_action :authenticate_user!
   def index
     if user_signed_in?
       redirect_to roasts_path
